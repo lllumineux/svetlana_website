a5b01dea1c737a23c640cc5f5a2ff7c3d97202af
Ramil Suleymanov <lllumineux@outlook.com>
2021-08-25 00:50:41 +0300

    Customize list users method

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,10 +1,21 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from apps.accounts import models, serializers
+from apps.courses.serializers import CourseSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserSerializer
     queryset = models.User.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        users = models.User.objects.all()
+        users_serialized = []
+        for user in users:
+            user_serialized = serializers.UserSerializer(user).data
+            user_serialized["courses"] = [CourseSerializer(obj.course).data for obj in models.UserCourse.objects.filter(user=user)]
+            users_serialized.append(user_serialized)
+        return Response(users_serialized)
 
 
 class UserCourseViewSet(viewsets.ModelViewSet):
